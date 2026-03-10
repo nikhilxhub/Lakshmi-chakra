3e2131d09423732f75aeb62bb3da76407182f295
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2026-03-10 23:36:53 +0530

    first things first

--- lakshmi-chakra/programs/lakshmi-chakra/src/lib.rs
+++ lakshmi-chakra/programs/lakshmi-chakra/src/lib.rs
@@ -6,11 +6,44 @@ declare_id!("BYe1eVU9XeUeezxyrUN7L9zfWBhcjGAYugmbzhf6L1ze");
 pub mod lakshmi_chakra {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
+
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+#[account]
+#[delegate]
+pub struct Lottery {
+    pub p0: f64,
+    pub k: f64,
+    pub total_sol: u64,
+
+    pub total_tickets: f64,
+    pub start_time: i64,
+    pub end_time: i64,
+    pub authority: Pubkey,
+    pub winner: Option<Pubkey>,
+    pub bump: u8,
+}
+
+#[account]
+#[delegate]
+pub struct UserTicket {
+    pub owner: Pubkey,
+    pub tickets: f64,
+    pub bump:u8,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Lottery has already ended")]
+    LotteryEnded,
+    #[msg("Lottery has not started yet")]
+    LotteryNotStarted,
+    #[msg("Not enough tickets")]
+    NotEnoughTickets,
+    #[msg("Invalid Sol amount")]
+    InvalidAmount,
+    #[msg("Invalid lottery parameters")]
+    InvalidLotteryParameters,
+}
+
+
